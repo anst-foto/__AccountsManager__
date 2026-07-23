32cca238100bc744b85213e35f9434185abdeaf9
Starinin Andrey <starininandrey@gmail.com>
2026-07-23 21:10:37 +0300

    Upgrade InputComponent

--- AccountsManager/Components/InputComponent/InputComponent.axaml.cs
+++ AccountsManager/Components/InputComponent/InputComponent.axaml.cs
@@ -5,22 +5,42 @@ namespace AccountsManager.Components;
 
 public partial class InputComponent : UserControl
 {
-    public static readonly StyledProperty<object> LabelProperty = 
+    public static readonly StyledProperty<object> LabelProperty =
         AvaloniaProperty.Register<InputComponent, object>(nameof(Label));
+
     public object Label
     {
         get => GetValue(LabelProperty);
         set => SetValue(LabelProperty, value);
     }
-    
-    public static readonly StyledProperty<string?> ValueProperty = 
-            AvaloniaProperty.Register<InputComponent, string?>(nameof(Value));
+
+    public static readonly StyledProperty<string?> ValueProperty =
+        AvaloniaProperty.Register<InputComponent, string?>(nameof(Value));
+
     public string? Value
     {
         get => GetValue(ValueProperty);
         set => SetValue(ValueProperty, value);
     }
-    
+
+    public static readonly StyledProperty<string?> PlaceholderProperty =
+        AvaloniaProperty.Register<InputComponent, string?>(nameof(Placeholder));
+
+    public string? Placeholder
+    {
+        get => GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsReadOnlyProperty =
+        AvaloniaProperty.Register<InputComponent, bool>(nameof(IsReadOnly));
+
+    public bool IsReadOnly
+    {
+        get => GetValue(IsReadOnlyProperty);
+        set => SetValue(IsReadOnlyProperty, value);
+    }
+
     public InputComponent()
     {
         InitializeComponent();
